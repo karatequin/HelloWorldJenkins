dc26a395e8abf65085c17936620873f22df406bf
Gustav <Gustavrodsgaard@gmail.com>
2019-04-29 14:37:22 +0200

    Initial commit

---
+++ src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class HomeController {
+
+    @GetMapping("/")
+    public String home(){
+        return "index";
+    }
+}
