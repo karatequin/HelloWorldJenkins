d4771483d2523394d0ca4a5e3ad1e8f3bd252db3
Gustav <Gustavrodsgaard@gmail.com>
2019-05-13 12:56:23 +0200

    Interns first commit

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -6,7 +6,7 @@ public class HomeController {
 
     @GetMapping("/")
     public String home(){
-        return "index";
+        return "index intern was here, sorry";
     }
 
 }
---
+++ src/test/java/com/example/demo/HomeControllerTest.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class HomeControllerTest {
+
+    //her tester vi udenfor Spring context:
+    private HomeController homeController = new HomeController();
+
+    @Test
+    public void home() {
+        assertEquals("index", homeController.home());
+    }
+}
